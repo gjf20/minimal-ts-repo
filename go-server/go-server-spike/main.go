eac3c95e03f22732e1562d744c9e888157a50a65
Jamie Flynn <gregoryjamesflynn@gmail.com>
2024-03-04 09:20:42 -0700

    feat(http-server): handlers

--- go-server/go-server-spike/main.go
+++ go-server/go-server-spike/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"example.com/db"
 )
 
-func main() {
-	fmt.Println("hello world")
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello world")
+}
 
+func fooHandler(w http.ResponseWriter, r *http.Request) {
 	things := db.ReadThings()
-	fmt.Println(things)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(things)
+}
+
+func main() {
+
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/foo", fooHandler)
+
+	http.ListenAndServe(":8090", nil)
 }
